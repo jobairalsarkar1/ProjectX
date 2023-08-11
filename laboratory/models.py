85b4c1182b3504d2bed892826d3439b443dd390b
Jobair Al Sarkar <jobairalsarkar338@gmail.com>
2023-08-11 16:24:40 +0600

    Models for lab is added.

--- laboratory/models.py
+++ laboratory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from doctors.models import Doctor
+from patients.models import Patient
 
-# Create your models here.
+class LabTest(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class LabResult(models.Model):
+    test = models.ForeignKey(LabTest, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    result = models.TextField()
+    date = models.DateTimeField()
