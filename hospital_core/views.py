1d9eff00237079b9f6383045b7da8a8567a7b939
Jobair Al Sarkar <jobairalsarkar338@gmail.com>
2023-08-17 21:38:48 +0600

    Test commit in testing Branch.

--- hospital_core/views.py
+++ hospital_core/views.py
@@ -5,11 +5,18 @@ from django.http import HttpResponse
 def landing_view(request):
     return render(request, 'landing.html')
 
+
 def blog(request):
     return render(request, 'blog.html')
+
 
 def service(request):
     return render(request, 'service.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
+
+def PatientRegister(request):
+    return render(request, 'base.html')
